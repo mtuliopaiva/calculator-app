e48be8845a62c92090c28594430ea88c48755006
Marco Túlio Sá de Paiva <tulio.paiva10@gmail.com>
2022-08-04 07:11:57 -0300

    Tarbalhando com adição no JS

--- main.js
+++ main.js
@@ -7,7 +7,33 @@ for(let i=0 ; i<digitos.length ;i++){
     const tecla = digitos[i];
 
     tecla.onclick = function() {
-        console.log(digitos[i]);
-    }
+        var operacao;
+        var valorAnterior;
+        var valorNovo;
+        var resultado;
 
+        if((tecla.value != 'C' && (tecla.value != '+/-') && (tecla.value != '%') && (tecla.value != '÷') && (tecla.value != 'x') && (tecla.value != '-') && (tecla.value != '+') && (tecla.value != '='))){
+        display.value = display.value + tecla.value;
+        valorAnterior=display.value;
+        console.log(valorAnterior);
+        
+        }
+
+        if(tecla.value == '+'){
+            display.value ='';
+
+            if((tecla.value != 'C' && (tecla.value != '+/-') && (tecla.value != '%') && (tecla.value != '÷') && (tecla.value != 'x') && (tecla.value != '-') && (tecla.value != '+') && (tecla.value != '='))){
+                display.value = display.value + tecla.value;
+                valorNovo=display.value;
+                console.log(valorAnterior);
+                console.log(valorNovo);
+            }   
+        }
+    }
+    function adicionar(valorAnterior, valorNovo){
+        resultado = parseInt(valorAnterior) + parseInt(valorNovo);
+        
+        
+        
+    }
 }
